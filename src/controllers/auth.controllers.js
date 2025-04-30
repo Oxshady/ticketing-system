4862928774c4013430946fd2e216fc667bbcc9ef
Oxshady <shadymahmod55@gmail.com>
2025-04-30 18:45:11 +0300

    adding controllers of open authentication

---
+++ src/controllers/auth.controllers.js
@@ -0,0 +1,61 @@
+const {createUser, foundUser} = require('../services/user.services')
+const {comparePassword} = require('../utils/password.utils')
+const {createToken} = require('../utils/jwtHelper.utils')
+const loginController = async (req, res) => {
+	const { email , password } = req.body
+	const user = await foundUser(email)
+	if (!user)
+		return res.json({
+			msg: "user not found"
+		})
+	if (!await comparePassword(user.password, password))
+		return res.json({
+		msg: "wrong password"
+		})
+		
+	const token = await createToken({
+		id: user.id,
+		email: user.email
+	})
+	return res.json({
+		token
+	})
+}
+
+const registerConstoller = async (req, res) => {
+	const {
+		firstName,
+		lastName,
+		email,
+		password
+	} = req.body;
+	if (await foundUser(email)){
+		return res.json({
+			msg:"user already found"
+		})
+	}
+	const user = await createUser(firstName, lastName, email, password)
+	const token = await createToken({
+		id: user.id,
+		email: user.email
+	})
+	return res.json({
+		token
+	})
+}
+
+const googleAuth = async (req, res)=>{
+	const token = await createToken({
+		id: req.user.id,
+		email: req.user.email
+	})
+	return res.json({
+		token
+	})
+}
+
+module.exports = {
+	registerConstoller
+	,loginController,
+	googleAuth
+}
