32a574eb56cc3af5e65f7df98efa6572eefa8121
Oxshady <shadymahmod55@gmail.com>
2025-04-30 18:45:37 +0300

    adding routes of open authentication

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,11 +1,12 @@
 const auth = require('express').Router()
+const passport = require('passport')
+const passportConfig = require('../config/passportConfig')
 
-auth.get('/login', (req, res) => {
-	const {
-		firstName,
-		lastName,
-		email,
-		password
-	} = req.body
-	
-})
+const { registerConstoller, loginController, googleAuth } = require('../controllers/auth.controllers')
+auth.post('/register', registerConstoller)
+auth.post('/login', loginController)
+auth.get('/google', passport.authenticate('google', {scope: ['profile', 'email'], session: false}))
+auth.get('/google/callback', passport.authenticate('google', {failureRedirect: '/login', session:false}), googleAuth)
+module.exports = {
+	auth
+}
