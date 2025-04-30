a7b4f669eb3e52f75b3a61eee8ac680ea2d9c3c4
Oxshady <shadymahmod55@gmail.com>
2025-04-30 19:02:00 +0300

    putting validation middlware to auth controller

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,11 @@
 const auth = require('express').Router()
 const passport = require('passport')
 const passportConfig = require('../config/passportConfig')
+const {registerValidator, loginValidator} = require('../middlewares/auth.validator')
 
 const { registerConstoller, loginController, googleAuth } = require('../controllers/auth.controllers')
-auth.post('/register', registerConstoller)
-auth.post('/login', loginController)
+auth.post('/register', registerValidator(),registerConstoller)
+auth.post('/login', loginValidator() ,loginController)
 auth.get('/google', passport.authenticate('google', {scope: ['profile', 'email'], session: false}))
 auth.get('/google/callback', passport.authenticate('google', {failureRedirect: '/login', session:false}), googleAuth)
 module.exports = {
