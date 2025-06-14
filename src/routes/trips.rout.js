939652a72c6f1af7afdfed5c670592c0f7acbad8
Oxshady <shadymahmod55@gmail.com>
2025-06-14 20:53:55 +0300

    change to trip seats post

--- src/routes/trips.rout.js
+++ src/routes/trips.rout.js
@@ -3,7 +3,7 @@ const { tripController, tripSeats } = require('../controllers/trip.controllers')
 const catchAsync = require('../utils/asyncWrapper.utils');
 
 tripsRouter.get('/', tripController);
-tripsRouter.get('/seats', tripSeats)
+tripsRouter.post('/seats', tripSeats)
 module.exports = {
 	tripsRouter
 };
